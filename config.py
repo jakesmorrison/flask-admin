c512dbed9b4c904a5452504c5d1ff7d660307a29
Jake <jakesmorrison4@gmail.com>
2022-03-28 10:14:51 -0600

    update

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
-SQLALCHEMY_DATABASE_URI = "sqlite:///home/database/oscar.sqlite3"
+# SQLALCHEMY_DATABASE_URI = "sqlite:///home/database/oscar.sqlite3"
+SQLALCHEMY_DATABASE_URI = "sqlite:///../database/oscar.sqlite3"
